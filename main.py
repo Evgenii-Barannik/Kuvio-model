5aef0b5c51f474cf3fa1a256d2a6d7ed295a4328
Evgenii-Barannik <radchemsu@gmail.com>
2024-01-17 23:49:03 +0300

    Resource weights

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ from typing import Optional, Dict
 from dataclasses import dataclass, field
 from math import log
 
-UTILITY_SHIFT = 1
+RESOURCE_BIAS = 1
+
+RESOURCE_WEIGHTS = {
+    "gold": 1,
+    "wood": 1,
+    "ore": 1,
+    "mercury": 5,
+    "sulfur": 5,
+    "crystal": 5,
+    "gem": 5,
+}
 
 @dataclass
 class Resources:
@@ -24,36 +34,37 @@ class Resources:
 
     def update(self, changed_resources:  Optional[Dict[str, int]] = None) -> None:
         if changed_resources is not None:
+            changes_strings = []
             for name, new_resource_amount in changed_resources.items():
                 current_resource_amount = self._resources.get(name, 0)
                 
-                changes_string = "Resource changes: {"
                 if current_resource_amount != new_resource_amount:
                     self._resources[name] = new_resource_amount
-                    changes_string = changes_string + "{}: {} -> {}".format(name, current_resource_amount, new_resource_amount)
-                changes_string = changes_string + " }\n"
-                print(changes_string)
+                    changes_strings.append("{}: {} -> {}".format(name, current_resource_amount, new_resource_amount))
+            print("Resources changed: {{ {} }}\n".format(", ".join(changes_strings)))
 
     def shift(self, name: str, delta: int) -> None:
         current_resource_amount = self._resources.get(name, 0)
         new_resource_amount = current_resource_amount + delta
         self._resources[name] = new_resource_amount
-        print("Resource changes: {{ {}: {} -> {} }}\n".format(name, current_resource_amount, new_resource_amount))
+        print("Resources changed: {{ {}: {} -> {} }}\n".format(name, current_resource_amount, new_resource_amount))
 
-    # We add UTILITY_SHIFT because without it resource change 0 -> 1 will not change utility value.
+    # We add RESOURCE_BIAS because without it resource change 0 -> 1 will not change utility.
     # It is so because log(1) == 0.0.
     def utility(self) -> float:
         total_utility = 0.0
-        for resource_name, amount in self._resources.items():
-            total_utility += (log(amount) + UTILITY_SHIFT if amount > 0 else 0.0) 
+        for resource_name, resource_amount in self._resources.items():
+            resource_weight = RESOURCE_WEIGHTS.get(resource_name, 1)
+            if resource_amount > 0:
+                total_utility += (resource_weight*(log(resource_amount) + RESOURCE_BIAS))
         return total_utility
 
 
 if __name__ == "__main__": 
     test_resources = Resources({
-        "gold": 5,
-        # "wood": 0,
-        # "ore": 0,
+        "gold": 1,
+        # "wood": 1,
+        # "ore": 3,
         # "mercury": 0,
         # "sulfur": 0,
         # "crystal": 0,
@@ -61,9 +72,9 @@ if __name__ == "__main__":
     })
     print(test_resources)
 
-    test_resources.shift("gold", 10)
+    test_resources.shift("gold", 9)
     print(test_resources)
 
-    test_resources.update({"gems": 1, "mercury": 1})
+    test_resources.update({"gem": 1, "mercury": 1})
     print(test_resources)
     
