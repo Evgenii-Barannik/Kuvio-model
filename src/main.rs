3319100a19bb685fd146ef1b28bb9d764f30a9ff
Evgenii-Barannik <radchemsu@gmail.com>
2024-03-30 16:22:24 +0200

    New architecture works!

--- src/main.rs
+++ src/main.rs
@@ -24,57 +24,59 @@ use rayon::prelude::*;
 // use rayon::ThreadPoolBuilder;
 use std::iter::zip;
 
-// type AgentID = usize;
-
 #[derive(PartialEq, PartialOrd, Eq, Ord, Debug, Hash, Clone, EnumIter)]
 enum Resource {
     Coins,
 }
 
+type AgentID = usize;
+
 type Resources = BTreeMap<Resource, usize>;
+
 type Action = fn(&mut Resources);
-type ActionVecModification = fn(&mut Vec<Action>);
-type GameProvider = fn() -> Game;   
-type GameAssigner = fn(&Game, Vec<Agent>) -> BTreeMap<AnyRole, &Agent>;
-type Decider = fn(Vec<Action>, DecisionMakingData, &mut StdRng) -> Action;
+type ActionTransformer = fn(&mut Vec<Action>);
+type ActionDecider = fn(Vec<Action>, DecisionMakingData, &mut StdRng) -> Action;
 type DecisionMakingData = Vec<f64>;
+
+type GameProvider = fn() -> Game;   
+type GameAssigner = fn(&Game, &Vec<Agent>) -> BTreeMap<AnyRole, AgentID>;
 
 #[derive(Clone)]
 struct Agent {
+    _id: AgentID,
     resources: Resources,
     actions: Vec<Action>,
-    decider: Decider,
+    decider: ActionDecider,
 }
 
 impl Agent {
-    fn new(initial_resources: Resources, actions: Vec<Action>, decider: Decider) -> Agent {
+    fn new(initial_resources: Resources, actions: Vec<Action>, decider: ActionDecider, _id: AgentID) -> Agent {
         let mut zeroed_resources = Resource::iter().map(|r| (r, 0)).collect::<Resources>();
         for (resource, amount) in initial_resources {
             zeroed_resources.insert(resource, amount);
         }
 
-        Agent {resources: zeroed_resources, actions, decider}
+        Agent {resources: zeroed_resources, actions, decider, _id}
     }
 
 }
 
 struct Game  {
-    mods: BTreeMap<AnyRole, ActionVecModification>,
+    mods: BTreeMap<AnyRole, ActionTransformer>,
 }
 
 impl Game {
-    fn transform_actions(&self, assigned_roles: &BTreeMap<AnyRole, usize>, agents: Vec<Agent>) -> Vec<Action> {
-        let mut all_modified_actions: Vec<Action> = Vec::new(); 
+    fn transform_actions(&self, assigned_roles: &BTreeMap<AnyRole, AgentID>, ordered_agents: &Vec<Agent>) -> BTreeMap<AgentID, Vec<Action>> {
+        let mut all_transformed_actions: BTreeMap<AgentID, Vec<Action>> = BTreeMap::new(); 
 
         for (assigned_role, agent_id) in assigned_roles.iter() {
-            if let  Some(action_modifier) = self.mods.get(assigned_role) {
-                let mut cloned_actions = agents[*agent_id].actions.clone();
-                action_modifier(&mut cloned_actions);
-                return cloned_actions
+            if let Some(action_transformer) = self.mods.get(assigned_role) {
+                let mut cloned_actions = ordered_agents[*agent_id].actions.clone();
+                action_transformer(&mut cloned_actions);            
+                all_transformed_actions.insert(*agent_id, cloned_actions);
             } 
-            // TODO Make work for multiple agents alltogether (with slices?)
         }
-        panic!("Modied actions not created.")
+        all_transformed_actions
     }
 }
 
@@ -106,21 +108,34 @@ enum AnyRole {
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone)]
 enum KingdomRole {
     King,
+    Peasant,
 }
 
-fn pass(res: &mut Resources) {}
+fn pass_action(res: &mut Resources) {}
 
-const TRIVIAL_ACTIONS: [Action; 1] = [pass];
-
-fn trivial_modification(actions: &mut Vec<Action>) {
+fn mint_action(res: &mut Resources) {
+    *res.entry(Resource::Coins).or_insert(0) += 10;
 }
 
-fn mint(res: &mut Resources) {
+fn work_action(res: &mut Resources) {
     *res.entry(Resource::Coins).or_insert(0) += 1;
 }
 
-fn add_mint(actions: &mut Vec<Action>) {
-    actions.push(mint)
+fn trivial_trasformer(_actions: &mut Vec<Action>) {}
+
+fn add_mint_transformer(actions: &mut Vec<Action>) {
+    actions.push(mint_action)
+}
+
+fn add_work_transformer(actions: &mut Vec<Action>) {
+    actions.push(work_action)
+}
+
+fn kingdom_game_provider() -> Game {
+    let mut mods = BTreeMap::new();
+    mods.insert(AnyRole::KingdomRole(KingdomRole::King), add_mint_transformer as ActionTransformer);
+    mods.insert(AnyRole::KingdomRole(KingdomRole::Peasant), add_work_transformer as ActionTransformer);
+    Game {mods: mods}
 }
 
 fn weighted_rng_decider(actions: Vec<Action>, data: DecisionMakingData, rng: &mut StdRng) -> Action {
@@ -129,17 +144,11 @@ fn weighted_rng_decider(actions: Vec<Action>, data: DecisionMakingData, rng: &mu
     actions[chosen_index].clone()
 }
 
-fn mint_game_provider() -> Game {
-    let mut mods = BTreeMap::new();
-    mods.insert(AnyRole::KingdomRole(KingdomRole::King), add_mint as ActionVecModification);
-    Game {mods: mods}
-}
-
-fn trivial_assigner(game: &Game, agents: Vec<Agent>) -> BTreeMap<AnyRole, usize> {
-    let mut assigned_agents: BTreeMap<AnyRole, usize> = BTreeMap::new();
-    
+fn index_checking_assigner(game: &Game, agents: &Vec<Agent>) -> BTreeMap<AnyRole, AgentID> {
+    let mut assigned_agents: BTreeMap<AnyRole, AgentID> = BTreeMap::new();
     for (role, index) in game.mods.keys().zip(0..agents.len()) {
-        assigned_agents.insert(role.to_owned(), index);
+        let agent_id = agents[index]._id;
+        assigned_agents.insert(role.to_owned(), agent_id);
     }
     
     assigned_agents
@@ -168,34 +177,46 @@ fn main() {
     let kingdom_subselection_factor: usize = 1;
     let seed: usize = 2;
 
-    let mut reputations = vec![vec![1f64; num_of_agents]; num_of_agents];
+    let reputations = vec![vec![1f64; num_of_agents]; num_of_agents];
     let mut rng = StdRng::seed_from_u64(seed as u64);
     let mut tile = Tile::new(vec![], BTreeMap::new(), reputations);
-
+    
     for i in 0..num_of_agents {
         let agent = Agent::new(
             BTreeMap::new(),
-            TRIVIAL_ACTIONS.to_vec(),
-            weighted_rng_decider,
+            [pass_action as Action; 1].to_vec(),
+            weighted_rng_decider as ActionDecider,
+            i as AgentID,
         );
         tile.agents.push(agent);
     }
-
     
+    let provider: GameProvider = kingdom_game_provider;
+    let assigner: GameAssigner = index_checking_assigner;
+
     for tick in 0..num_of_ticks {
         let mut games: Vec<Game> = vec![];
         if (tick % kingdom_subselection_factor) == 0 {
-            games.push(mint_game_provider());
+            games.push(provider());
         }
+        use rand::prelude::SliceRandom;
         
-        for game in games {
-            let assigned_agents = trivial_assigner(&game, tile.agents.clone());
-            let new_actions = game.transform_actions(&assigned_agents, tile.agents.clone());
-            let normalized_vector = generate_normalized_vector(&mut rng, new_actions.len());
-            let choosen_action = weighted_rng_decider(new_actions, normalized_vector, &mut rng); 
+        let shuffled_agents = {
+            let mut agents = tile.agents.clone();
+            agents.shuffle(&mut rng);
+            agents
+        };
 
-            let agent_index = *assigned_agents.first_key_value().unwrap().1; // Dereference to get usize directly
-            choosen_action(&mut tile.agents[agent_index].resources); // Use usize to index
+        for game in games {
+            let assigned_agent_ids = assigner(&game, &shuffled_agents);
+            let new_actions = game.transform_actions(&assigned_agent_ids, &tile.agents);
+
+            for (agent_id, actions) in new_actions {
+                let decider = tile.agents[agent_id].decider;
+                let decider_data = generate_normalized_vector(&mut rng, actions.len());
+                let choosen_action = decider(actions, decider_data, &mut rng); 
+                choosen_action(&mut tile.agents[agent_id].resources); 
+            } 
        }
 
        let mut summary_log = String::new();
